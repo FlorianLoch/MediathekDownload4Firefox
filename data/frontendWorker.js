35ccad1f7c9f4f80d364fdb713bfe243ed05d56c
Florian Loch <Florian.Loch@gmail.com>
2013-10-04 13:40:29 +0200

    First completly working prototype Now fully functional, but UI still needs to be polished. Improved the isHandlerForURL()-method of the ZDFHandler (using regular expressions). Loading animation while requesting and parsing page. If the user isn't on a recognized page, the addon will tell him when ran. If the user visits a recognized page, the icon and text of the widget will change according to the mediathek that has been recognized.

--- data/frontendWorker.js
+++ data/frontendWorker.js
@@ -1,11 +1,37 @@
-self.port.on("show", function (oMediathek) {
-	
+self.port.on("showLoading", function (oMediathek) {
+	setLayer("#loadingLayer");
 });
 
 self.port.on("showNoMediathek", function (oMediathek) {
-	
+	setLayer("#noMediathekLayer");
 });
 
 self.port.on("showURLs", function (oVideoURLs) {
+	var node = $("#selectionLayer");
+	node.html("");
+	
+	for (var i = 0; i < oVideoURLs.length; i++) {
+		var btn = $("<button>" + oVideoURLs[i].desc + "</button>");
+
+		//This (complex) ways is needed because otherwise the data would just be 
+		//referenced, not copied - this would result in problems because of iterating		
+		(function (oURL) {
+			btn.click(function () {
+					self.port.emit("URLSelected", {url: oURL.url, name: oURL.desc});	
+			});
+		})(oVideoURLs[i]);
 		
+		node.append(btn);
+	}
+		
+	setLayer("#selectionLayer");
 });
+
+function setLayer (sLayerId) {
+	$("div").hide();
+	$(sLayerId).show();	
+}
+
+function download() {
+	alert("Hallo");
+};
