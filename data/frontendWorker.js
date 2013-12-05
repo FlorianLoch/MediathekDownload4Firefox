ea19d0f3b4bca071ad2a87c69da3160f63865b1a
Florian Loch <Florian.Loch@gmail.com>
2013-12-05 17:02:31 +0100

    Rewrite of frontendWorker.js, HD support for ZDF, jquery.min Because of rejection by mozilla I had to rewrite the way the frontend is built - it is no longer built using .html(), it is now built by native and pure DOM-manipulation. These might not be pretty but hopefully Mozilla likes this version more. Furthermore there is now support for the HD videos of ZDF - also there are just a few of these. Also I changed from jquery.1.10 back to jquery.1.10.min.

--- data/frontendWorker.js
+++ data/frontendWorker.js
@@ -1,27 +1,48 @@
+//I know this code for DOM-generating might not by great (some things could be refactored and moved to functions etc.),
+//but before putting time (which I do not have at the moment due to studying) into this I would like to publish this "dirty" but perfectly working version.
+//I will clean it up a in following release.
+
 self.port.on("showLoading", function (oMediathek) {
 	$("div").hide();
-	$("#selectionLayer").html("");
+	$("#selectionLayer").empty();
 	
 	$("#loadingLayer").show();
 });
 
 self.port.on("showNoMediathek", function (arMediatheken) {
-	var out = "Diese Seite konnte leider keiner Mediathek zugeordnet werden.<br>";
-	out += "<div id='mediathekenList'>Folgende Mediatheken werden unterstützt:";
-	out += "<ul>";
-	for (var i = 0; i < arMediatheken.length; i++) {
-		out += "<li><a target='_blank' href='" + arMediatheken[i].url + "'>" + arMediatheken[i].name + "</a></li>";
+	var noMedLay = document.getElementById("noMediathekLayer");
+	for (var i = 0; i < noMedLay.childNodes.length; i++) {
+		noMedLay.removeChild(noMedLay.childNodes[i]);
 	}
-	out += "</ul></div>";
+	noMedLay.appendChild(document.createTextNode("Diese Seite konnte leider keiner Mediathek zugeordnet werden"));
+	noMedLay.appendChild(document.createElement("br"));
+	var listDiv = document.createElement("div");
+	listDiv.setAttribute("id", "mediathekenList");
+	listDiv.appendChild(document.createTextNode("Folgende Mediatheken werden unterstützt:"));
+	var listUl = document.createElement("ul");
+
+	for (var i = 0; i < arMediatheken.length; i++) {
+		var li = document.createElement("li");
+		var a = document.createElement("a");
+		a.setAttribute("target", "_blank");
+		a.setAttribute("href", escapeHTML(arMediatheken[i].url)); 
+		a.appendChild(document.createTextNode(escapeHTML(arMediatheken[i].name)));
+		li.appendChild(a);
+
+		listUl.appendChild(li);
+	}
+
+	listDiv.appendChild(listUl);
+	noMedLay.appendChild(listDiv);
 		
-	$("#noMediathekLayer").html(out);
-		
-	setLayer("#noMediathekLayer");
+	setLayer("#noMediathekLayer", 0);
 });
 
 self.port.on("showURLs", function (oVideoURLs) {
-	var node = $("#selectionLayer");
-	node.html("");
+	var node = document.getElementById("selectionLayer");
+	for (var i = 0; i < node.childNodes.length; i++) {
+		node.removeChild(node.childNodes[i]);
+	}
 	
 	if (oVideoURLs == null || oVideoURLs.length == 0) {
 		var text = "";
@@ -35,36 +56,41 @@ self.port.on("showURLs", function (oVideoURLs) {
 		
 		text += "<br>Das tut uns Leid!<br><img id='sadPingu' src='sad_pinguin.svg'>";
 		
-		node.html(text);
-		setLayer("#selectionLayer");
+		node.innerHTML = text; //safe, because only predefined, constant string
+		setLayer("#selectionLayer", 300);
 
 		return;		
 	}
 	
 	for (var i = 0; i < oVideoURLs.length; i++) {
-		var btn = $("<button class='btn btn-primary btn-download'>" + oVideoURLs[i].desc + " (" + round(oVideoURLs[i].fs / 1024 / 1024) + " mb)</button>");
+		if (oVideoURLs[i].exists == true) {
+			//var btn = $("<button class='btn btn-primary btn-download'>" + escapeHTML(oVideoURLs[i].desc) + " (" + round(escapeHTML(oVideoURLs[i].fs) / 1024 / 1024) + " mb)</button>");
+			var btn = document.createElement("button");
+			btn.setAttribute("class", "btn btn-primary btn-download");
+			btn.appendChild(document.createTextNode(escapeHTML(oVideoURLs[i].desc) + " (" + round(escapeHTML(oVideoURLs[i].fs) / 1024 / 1024) + " mb)"));
 
-		//This (complex) ways is needed because otherwise the data would just be 
-		//referenced, not copied - this would result in problems because of iterating		
-		(function (oURL) {
-			btn.click(function () {
-					self.port.emit("URLSelected", {url: oURL.url, name: oURL.desc});	
-			});
-		})(oVideoURLs[i]);
-		
-		node.append(btn);
+			//This (complex) way is needed because otherwise the data would just be 
+			//referenced, not copied - this would result in problems because of iterating		
+			(function (oURL) {
+				btn.addEventListener("click", function () {
+						self.port.emit("URLSelected", {url: escapeHTML(oURL.url), name: escapeHTML(oURL.desc)});	
+				}, false);
+			})(oVideoURLs[i]);
+			
+			node.appendChild(btn);
+		}
 	}
 		
-	setLayer("#selectionLayer");
+	setLayer("#selectionLayer", 300);
 });
 
-function setLayer (sLayerId) {
+function setLayer (sLayerId, duration) {
 	$("body > div:not(" + sLayerId + ")").fadeOut({
-			duration: 400,
+			duration: duration,
 			complete: function() {
 				setTimeout(function() {
-						$(sLayerId).delay(300).fadeIn({
-								duration: 400,
+						$(sLayerId).delay(duration).fadeIn({
+								duration: duration,
 								done: function () {
 									//self.port.emit("resizePanel", 330, $(sLayerId).height());
 								}
@@ -76,4 +102,8 @@ function setLayer (sLayerId) {
 
 function round(iVal) {
 	return Math.round(iVal * 100) / 100;	
+}
+
+function escapeHTML(sStr) {
+    return sStr.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
 }
